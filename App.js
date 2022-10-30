12ffb8f8d3919f8b59608c69292a8a1b24656d4a
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-10-30 13:27:11 +0300

    Working With Core Components

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world from KaleabIsDoneWithJava!!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>fIRST lINE oF tEXT!</Text>
+      </View>
+      <Text>Hello World!</Text>
+      <Button title="Tap Me Daddy"/>
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
