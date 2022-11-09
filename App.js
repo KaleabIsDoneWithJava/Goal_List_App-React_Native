1907fe2f41827957c6c05a6f36ca17b4768571f7
KaleabIsDoneWithJava <kaleab.teshome16@gmail.com>
2022-11-09 17:37:15 +0300

    App Finishing Changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, FlatList, StyleSheet, View } from "react-native";
+import { Button, FlatList, StyleSheet, View, StatusBar } from "react-native";
 import GoalInput from "./components/GoalInput";
 
 import GoalItem from "./components/GoalItem";
@@ -14,7 +14,6 @@ export default function App() {
 
   function endAddGoalHandler() {
     setModalIsVisible(false);
-
   }
 
   function addGoalHandler(enteredGoalText) {
@@ -36,39 +35,48 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color="#5e0acc"
-        onPress={startAddGoalHandler}
-      />
-      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} />
+    //backgroundColor works only on android
+    <>
+      <StatusBar style="auto" backgroundColor="#605f63"  />
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="#5e0acc"
+          onPress={startAddGoalHandler}
+        />
 
-      <View style={styles.goalsContainer}>
-        {/*Flatlist is like ScrollView but with Lazy Loading. It has most props that Scrollview 
+        <GoalInput
+          visible={modalIsVisible}
+          onAddGoal={addGoalHandler}
+          onCancel={endAddGoalHandler}
+        />
+
+        <View style={styles.goalsContainer}>
+          {/*Flatlist is like ScrollView but with Lazy Loading. It has most props that Scrollview 
         has and is better for performance when used with dynamic lists, especially when they're long*/}
 
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            //console.log(itemData.item.id)
-            //need to pass a prop into GoalItem to render data from the dynamic list.
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteItemHandler}
-              />
-            );
-          }}
-          //To get the key prop from another prop in the object passed to the data prop
-          keyExtractor={(items, index) => {
-            return items.id;
-          }}
-          alwaysBounceVertical={false}
-        />
+          <FlatList
+            data={courseGoals}
+            renderItem={(itemData) => {
+              //console.log(itemData.item.id)
+              //need to pass a prop into GoalItem to render data from the dynamic list.
+              return (
+                <GoalItem
+                  text={itemData.item.text}
+                  id={itemData.item.id}
+                  onDeleteItem={deleteItemHandler}
+                />
+              );
+            }}
+            //To get the key prop from another prop in the object passed to the data prop
+            keyExtractor={(items, index) => {
+              return items.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -6,6 +6,7 @@ import {
   Button,
   Modal,
   Image,
+  StatusBar,
 } from "react-native";
 
 function GoalInput(props) {
@@ -19,39 +20,52 @@ function GoalInput(props) {
     props.onAddGoal(enteredGoalText);
     setEnteredGoalText("");
   }
-  function closeModalHandler(){
+  function closeModalHandler() {
     props.onCancel();
     setEnteredGoalText("");
   }
 
   return (
-    <Modal visible={props.visible} animationType="slide">
-      <View style={styles.inputContainer}>
-        <Image
-          style={styles.image}
-          source={require("../assets/images/goal.png")}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Your Course Goal!"
-          placeholderTextColor="#ffffff"
-          onChangeText={goalInputHandler}
-          value={enteredGoalText}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} />
-          </View>
-          <View style={styles.button}>
-            <Button title="Cancel" onPress={closeModalHandler} />
+    <>
+      <Modal visible={props.visible} animationType="slide">
+        <View style={styles.inputContainer}>
+          <Image
+            style={styles.image}
+            source={require("../assets/images/goal.png")}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Your Course Goal!"
+            onChangeText={goalInputHandler}
+            value={enteredGoalText}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title="Cancel"
+                onPress={closeModalHandler}
+                color="#f31282"
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Add Goal"
+                onPress={addGoalHandler}
+                color="#b180f0"
+              />
+            </View>
           </View>
         </View>
-      </View>
-    </Modal>
+      </Modal>
+    </>
   );
 }
 
 export default GoalInput;
+
+/*Can't style the built in button. 
+You have to create your own button component and style it appropriately.
+ You should use the Pressable component */
 
 const styles = StyleSheet.create({
   inputContainer: {
@@ -70,10 +84,11 @@ const styles = StyleSheet.create({
 
   input: {
     borderWidth: 1,
-    borderColor: "#cccccc",
-    padding: 5,
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    padding: 16,
     width: "100%",
-    color: "#ffffff",
+    borderRadius: 20,
   },
 
   buttonContainer: {
@@ -84,5 +99,6 @@ const styles = StyleSheet.create({
   button: {
     marginHorizontal: 8,
     width: "30%",
+    borderRadius: 20,
   },
 });
